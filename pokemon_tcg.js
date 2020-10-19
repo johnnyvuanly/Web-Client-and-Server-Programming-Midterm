59ea4d9b2c99217b332376887a25155cae55db1e
John L <johnny@email.com>
2020-10-18 21:50:40 -0500

    attempt 10/18/20

--- pokemon_tcg.js
+++ pokemon_tcg.js
@@ -1,6 +1,21 @@
 // alert('Testing connection to html')
 
 let url = 'https://api.pokemontcg.io/v1/cards/'
-console.log(url)
 let url2 = 'https://api.pokemontcg.io/v1/sets'
-console.log(url2)
+
+console.log(url, url2)
+
+let cardInput = document.querySelector('#card')
+let setInput = document.querySelector('#set')
+let checkButton = document.querySelector('#check')
+let resultDisplay = document.querySelector('#answer')
+
+checkButton.addEventListener('click', function() {
+    let cards = cardInput.value
+    let sets = setInput.value
+
+    fetch(url).then( (res) => {
+        console.log(res)
+        return res.json()
+    })
+})
