e90926e0e52d2a6c918a8ec9a9e1a7d5bee14cf9
John L <johnny@email.com>
2020-10-20 20:39:13 -0500

    Attempt 10/20/20

--- pokemon_tcg.js
+++ pokemon_tcg.js
@@ -1,14 +1,16 @@
 // alert('Testing connection to html')
 
-let url = 'https://api.pokemontcg.io/v1/cards/'
-let url2 = 'https://api.pokemontcg.io/v1/sets'
+//let url = 'https://api.pokemontcg.io/v1/cards/'
+let url = 'https://api.pokemontcg.io/v1/cards?setCode'
 
-console.log(url, url2)
+console.log(url)
 
 let cardInput = document.querySelector('#card')
 let setInput = document.querySelector('#set')
 let checkButton = document.querySelector('#check')
 let resultDisplay = document.querySelector('#answer')
+
+// TODO display if the card the user searched for is legal or not
 
 checkButton.addEventListener('click', function() {
     let cards = cardInput.value
@@ -17,5 +19,11 @@ checkButton.addEventListener('click', function() {
     fetch(url).then( (res) => {
         console.log(res)
         return res.json()
+    }).then( cardData => {
+        console.log(cardData)
+        let findNumber = cardData.name
     })
 })
+
+// If user enters a promo card for the set then what should you do?
+// 
