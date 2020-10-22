47eee24f701a1ccd04cbebfc1b7c89620f6e7961
John L <johnny@email.com>
2020-10-21 21:33:38 -0500

    Finished up design

--- pokemon_tcg.js
+++ pokemon_tcg.js
@@ -1,10 +1,5 @@
 // alert('Testing connection to html')
 
-//let url = 'https://api.pokemontcg.io/v1/cards/'
-let url = 'https://api.pokemontcg.io/v1/cards?setCode'
-
-console.log(url)
-
 let cardInput = document.querySelector('#card')
 let setInput = document.querySelector('#set')
 let checkButton = document.querySelector('#check')
@@ -16,14 +11,29 @@ checkButton.addEventListener('click', function() {
     let cards = cardInput.value
     let sets = setInput.value
 
+    url = `https://api.pokemontcg.io/v1/cards?name=${cards}&set=${sets}`
+
     fetch(url).then( (res) => {
         console.log(res)
         return res.json()
     }).then( cardData => {
-        console.log(cardData)
-        let findNumber = cardData.name
+        console.log(cardData) // Shows the object of what the user inputed
+        // TODO check whether the card the user looked for is standard legal or not
+        // TODO display the image of the card
+        // cardData.forEach(function(element) {
+        //     element.imageUrlHiRes
+        // }) 
+    }).catch(error => {
+        console.log(error)
+        alert(`Couldn't connect to the pokemontcg.io API`)
     })
 })
 
 // If user enters a promo card for the set then what should you do?
 // 
+
+// https://api.pokemontcg.io/v1/cards?name=charizard
+// https://api.pokemontcg.io/v1/sets?name=dragon
+// https://api.pokemontcg.io/v1/cards?name=charizard&set=dragon
+// https://api.pokemontcg.io/v1/sets/sm75
+
